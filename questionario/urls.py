94c20aec93c5df2f5683c3bb8c3316ceb5e3cc54
Sávio Silva Moreira <saviodiowflamengo@gmail.com>
2020-12-15 21:02:59 -0300

    teste grafico

--- questionario/urls.py
+++ questionario/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 
-from questionario.views import quest, obrigado
+from questionario.views import quest, obrigado, rel_dif_temp
 
 urlpatterns = [
     path('', quest, name='quest'),
     path('obrigado/', obrigado, name='obrigado'),
+    path('relatorio/tempo_dificuldade/', rel_dif_temp, name='rel_dif_temp'),
+
 ]
--- questionario/views.py
+++ questionario/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 from questionario.forms import QuestForm
-
+from questionario.models import Quest
 
 
 def obrigado(request):
@@ -25,3 +25,94 @@ def quest(request):
 
 
     return render(request,'quest_form.html',context)
+
+
+def rel_dif_temp(request):
+    context={}
+
+    quests = list(Quest.objects.all())
+
+    list_1hora       = [0, 0, 0, 0, 0]
+    list_2horas      = [0, 0, 0, 0, 0]
+    list_3horas      = [0, 0, 0, 0, 0]
+    list_4horas      = [0, 0, 0, 0, 0]
+    list_4horas_mais = [0, 0, 0, 0, 0]
+
+    for q in quests:
+        if q.pergunta8 == '1':
+
+            if q.pergunta7 == '1':
+                list_1hora[0] += 1
+            if q.pergunta7 == '2':
+                list_1hora[1] += 1
+            if q.pergunta7 == '3':
+                list_1hora[2] += 1
+            if q.pergunta7 == '4':
+                list_1hora[3] += 1
+            if q.pergunta7 == '5':
+                list_1hora[4] += 1
+
+
+        elif q.pergunta8 == '2':
+
+            if q.pergunta7 == '1':
+                list_2horas[0] += 1
+            if q.pergunta7 == '2':
+                list_2horas[1] += 1
+            if q.pergunta7 == '3':
+                list_2horas[2] += 1
+            if q.pergunta7 == '4':
+                list_2horas[3] += 1
+            if q.pergunta7 == '5':
+                list_2horas[4] += 1
+
+        elif q.pergunta8 == '3':
+
+            if q.pergunta7 == '1':
+                list_3horas[0] += 1
+            if q.pergunta7 == '2':
+                list_3horas[1] += 1
+            if q.pergunta7 == '3':
+                list_3horas[2] += 1
+            if q.pergunta7 == '4':
+                list_3horas[3] += 1
+            if q.pergunta7 == '5':
+                list_3horas[4] += 1
+
+        elif q.pergunta8 == '4':
+
+            if q.pergunta7 == '1':
+                list_4horas[0] += 1
+            if q.pergunta7 == '2':
+                list_4horas[1] += 1
+            if q.pergunta7 == '3':
+                list_4horas[2] += 1
+            if q.pergunta7 == '4':
+                list_4horas[3] += 1
+            if q.pergunta7 == '5':
+                list_4horas[4] += 1
+
+        elif q.pergunta8 == '5':
+
+            if q.pergunta7 == '1':
+                list_4horas_mais[0] += 1
+            if q.pergunta7 == '2':
+                list_4horas_mais[1] += 1
+            if q.pergunta7 == '3':
+                list_4horas_mais[2] += 1
+            if q.pergunta7 == '4':
+                list_4horas_mais[3] += 1
+            if q.pergunta7 == '5':
+                list_4horas_mais[4] += 1
+
+
+    context['list_1hora'] = list_1hora
+    context['list_2horas'] = list_2horas
+    context['list_3horas'] = list_3horas
+    context['list_4horas'] = list_4horas
+    context['list_4horas_mais'] = list_4horas_mais
+
+
+    return render(request,'rel_dif_temp.html', context)
+
+
